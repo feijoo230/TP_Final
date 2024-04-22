7493597b32cca744cccca21d6e0572e1d07bc647
feijoo230 <105840516+feijoo230@users.noreply.github.com>
2024-04-22 14:19:39 -0300

    TP Final

---
+++ src/trabajoPracticoFinal/main.java
@@ -0,0 +1,17 @@
+package trabajoPracticoFinal;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+/**
+ *
+ * @author jesus
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+}
